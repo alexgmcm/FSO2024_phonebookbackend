2ffa30db19c0f2bd7bfc4bfa7d4e97aa72662124
Alexander McMurray <alexander.mcmurray@king.com>
2024-02-27 21:10:01 +0100

    ex 3.17 done

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/persons/:id', (request, response,next) => {
       name: body.name,
       number: body.number
     }
-    Person.findByIdAndUpdate(request.params.id,person, {new:true}).then(updatedPerson =>{
+    Person.findByIdAndUpdate(request.params.id,person, {new:true, runValidators:true,context:'query'}).then(updatedPerson =>{
       response.json(updatedPerson)
     }).catch(err=>next(err))
   })
@@ -44,7 +44,7 @@ app.get('/api/persons/:id', (request, response,next) => {
     .catch(err => next(err))
   })
 
-  app.post('/api/persons', (request, response) => {
+  app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (body.name === undefined) {
@@ -58,7 +58,7 @@ app.get('/api/persons/:id', (request, response,next) => {
 
     person.save().then(savedPerson => {
       response.json(savedPerson)
-    })
+    }).catch(err => next(err))
   })
 
   app.get('/info', (request, response) => {
@@ -84,7 +84,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -20,8 +20,15 @@ mongoose.connect(url)
 
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type:String,
+        minLength: 3,
+    
+    },
+    number: {
+        type:String,
+        minLength: 8,
+    }
   })
 
 personSchema.set('toJSON', {
